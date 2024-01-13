c348e56a85c9b74fd425b59fdc04f85586c479c1
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2024-01-13 12:51:20 +0100

    feat: :lipstick: Added Navbar to the UI

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import icon from "../assets/Modbus2ChainNoBackground.png";
+import styles from "./styles/Navbar.module.css"
+import { FaRegUserCircle } from "react-icons/fa";
+import { BiLogOutCircle } from "react-icons/bi";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+const Navbar = ({user}) => {
+  const [cookies, setCookie] = useCookies(["accessToken"]);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setCookie('accessToken', null);
+    navigate("/");
+  };
+
+  return (
+    <div className={styles.nav}>
+      <div className={styles.userDiv}>
+        <FaRegUserCircle style={{color:"white"}} size={50}/>
+        <p className={styles.pstyle} style={{fontSize:20}}>{user}</p>
+      </div>
+      <div className={styles.logoDiv}>
+        <img src={icon} style={{width:250}} alt="Modbus2Chain icon" />
+        <p className={styles.pstyle}>Modbus2Chain</p>
+      </div>
+      <BiLogOutCircle style={{color:"white",marginRight:80,cursor:"pointer"}} size={55} onClick={handleLogout}/>
+    </div>
+  );
+};
+
+export default Navbar;

d7723cf1f58a1b61c0edb04ad2f2d7675e59a697
Alessandro-Cavaliere <81223433+Alessandro-Cavaliere@users.noreply.github.com>
2024-01-13 12:50:08 +0100

    perf: :zap: Improved user experience by proper routing
    The routing has been managed in such a way that the non-logged in user will always see the login (or registration) if the path in the URL changes. Conversely, if you are logged in, you will always see the dashboard until your session expires or you log out.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 
-import React from "react";
+import React, { useState } from "react";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
-import Dashboard from "./components/Dashboard"; 
-
+import Dashboard from "./components/Dashboard";
+import "react-toastify/dist/ReactToastify.css";
 import styles from "./components/styles/Dashboard.module.css";
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { useCookies } from "react-cookie";
+
 function App() {
+  const [cookies, setCookie] = useCookies(["accessToken"]);
+  const [loggedUser, setLoggedUser] = useState("")
+
+  const handleLoggedUser = (email) => {
+     setLoggedUser(email)
+     console.log(email)
+  };
+
   return (
     <div className={styles.dashboard}>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/dashboard" element={<Dashboard />} /> 
+        {!cookies.accessToken ? (
+          <>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login handleLoggedUser={handleLoggedUser}/>} />
+            <Route path="/registration" element={<Registration />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard user={loggedUser}/>} />
+            <Route path="/login" element={<Navigate to="/dashboard" />} />
+            <Route path="/registration" element={<Navigate to="/dashboard" />} />
+          </>
+        )}
       </Routes>
+      <ToastContainer 
+        style={{ width: "auto" }}
+        position="bottom-center"
+        autoClose={1000}
+        theme="dark"
+        pauseOnHover
+      />
     </div>
   );
 }
